a7e783132bce84d40725f4b80e8cf8ef723c3a6a
Gabriel <gabri.elf@hotmail.com>
2021-03-12 10:22:35 -0300

    removendo arquivos desnecessários

--- components/Content.tsx
+++ components/Content.tsx
@@ -14,13 +14,6 @@ export const Content: React.FC<contentProps> = ({ title }) => {
                 <strong>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est ipsum perferendis qui, ab dolor praesentium!</strong>
                 <img src="/images/ocean-energy.svg" alt="" />
             </div>
-
-            <h4>{title}</h4>
-            <div className={styles.homePageContent}>
-                <h2>Layout Page with Next/Typescript</h2>
-                <strong>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est ipsum perferendis qui, ab dolor praesentium!</strong>
-                <img src="/images/ocean-energy.svg" alt="" />
-            </div>
         </div>
 
     )
